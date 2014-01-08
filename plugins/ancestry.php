0c65233abdcd73e93da91695bf066a64ef9b329f
JustCarmen <carmen@justcarmen.nl>
2014-01-08 07:23:00 +0100

    Ancestry plugin extended by Hermann

--- plugins/ancestry.php
+++ plugins/ancestry.php
@@ -12,18 +12,24 @@ class ancestry_plugin extends research_base_plugin {
 
 	static function create_link($primary_name) {
 		$domain = array(
-			'de'	=> 'de',
-			'en_GB' => 'co.uk',
-			'en_US' => 'com',
-			'it'	=> 'it',
+			'de'		=> 'de',		// German
+			'en_GB' 	=> 'co.uk',
+			'en_US'		=> 'com',
+			'en_AUS'	=> 'com.au',	// not used by webtrees
+			'fr'		=> 'fr',
+			'it'		=> 'it',
+			'sv'		=> 'se',		// Swedish
 			);
+		// ancestry supports Canada in English and French versions, too; but webtrees doesn't support these language versions
 		if (isset($domain[WT_LOCALE])) {
 			$ancestry_domain = $domain[WT_LOCALE];
 		} else {
 			$ancestry_domain = $domain['en_US'];
 		}
 
-		return $link = 'http://search.ancestry.'.$ancestry_domain.'/cgi-bin/sse.dll?new=1&gsfn='
+		return $link = 'http://search.ancestry.'
+						.$ancestry_domain
+						.'/cgi-bin/sse.dll?new=1&gsfn='
 						.rawurlencode($primary_name['givn'])
 						.'&gsln='
 						.rawurlencode($primary_name['surname'])
