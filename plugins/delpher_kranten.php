7b84563ccfc61bbf405424ad6f1081e7b1b5adf8
JustCarmen <carmen@justcarmen.nl>
2014-02-13 23:47:44 +0100

    New plugin added: Delpher Kranten (Dutch)

---
+++ plugins/delpher_kranten.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class delpher_kranten_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Delpher Krantenarchief';
+	}	
+	
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 'http://kranten.delpher.nl/nl/results?query="'.str_replace(" ","+",$fullname).'"';
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+}
