ad0bac5db919489d3548ade8488f8d872bcb85ab
JustCarmen <carmen@justcarmen.nl>
2014-02-14 00:04:09 +0100

    Remove debug code.

--- plugins/delpher_kranten.php
+++ plugins/delpher_kranten.php
@@ -11,7 +11,6 @@ class delpher_kranten_plugin extends research_base_plugin {
 	}	
 	
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
-		print_r($fullname);
 		return $link = 'http://kranten.delpher.nl/nl/results?query='.str_replace(" ", "+", $fullname);
 	}
 
