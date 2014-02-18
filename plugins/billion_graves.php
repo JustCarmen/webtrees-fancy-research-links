568fa41f59f40918d3bff9da6c0556754d11a833
JustCarmen <carmen@justcarmen.nl>
2014-02-18 20:37:34 +0100

    New plugin added: Billion Graves

---
+++ plugins/billion_graves.php
@@ -0,0 +1,26 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+    header('HTTP/1.0 403 Forbidden');
+    exit;
+}
+
+class billion_graves_plugin extends research_base_plugin {
+
+    static function getName() {
+        return 'Billion Graves';
+    }
+
+    static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+        return $link = 'http://billiongraves.com/pages/search/#given_names='
+                . rawurlencode($givn)
+                . '&family_names='
+                . rawurlencode($surname)
+                . '&birth_year=&death_year=&year_range=5&lim=0&num=10&action=search&exact=false&phonetic=true&record_type=0&country=0&state=0&county=0';
+    }
+
+    static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+        return false;
+    }
+
+}
