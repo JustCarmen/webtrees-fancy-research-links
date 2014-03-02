3f0ed46ddc610bd4cd1432630ee40c043be24ccd
JustCarmen <carmen@justcarmen.nl>
2014-03-02 08:06:26 +0100

    Explanation of used variables.

--- module.php
+++ module.php
@@ -167,14 +167,14 @@ class fancy_research_links_WT_Module extends WT_Module implements WT_Module_Conf
 						if($primary) {
 							
 							// create plugin vars						
-							$givn 		= $primary['givn'];
+							$givn 		= $primary['givn']; 														// all given names
 							$given		= explode(" ", $givn);
-							$first		= $given[0];
-							$middle		= count($given) > 1 ? $given[1] : "";
-							$surn 		= $primary['surn'];
-							$surname	= $primary['surname'];
-							$fullname 	= $givn.' '.$surname;
-							$prefix		= $surn != $surname ? substr($surname, 0, strpos($surname, $surn) - 1) : "";
+							$first		= $given[0]; 																// first given name
+							$middle		= count($given) > 1 ? $given[1] : "";										// middle name (second given name
+							$surn 		= $primary['surn'];															// surname without prefix
+							$surname	= $primary['surname'];														// full surname (with prefix)
+							$fullname 	= $givn.' '.$surname;														// full name
+							$prefix		= $surn != $surname ? substr($surname, 0, strpos($surname, $surn) - 1) : "";// prefix
 							
 							$link = $plugin->create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname);
 							$sublinks = $plugin->create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname);
