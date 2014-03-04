3a16d50ca0cdae92886fa8a0d0a2cf2dea9f1a83
JustCarmen <carmen@justcarmen.nl>
2014-03-04 06:54:48 +0100

    New plugin added 'Findmypast'.

---
+++ plugins/findmypast.php
@@ -0,0 +1,23 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class findmypast_plugin extends research_base_plugin {
+	static function getName() {
+		return 'findmypast';
+	}
+
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 'http://search.findmypast.com/search/world-records?firstname='
+						.rawurlencode($givn)
+						.'&firstname_variants=true&lastname='
+						.rawurlencode($surname);
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+}
