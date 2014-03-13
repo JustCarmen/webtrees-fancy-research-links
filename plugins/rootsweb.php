29c7238dc099399cfefea43647b8f2059792504e
JustCarmen <carmen@justcarmen.nl>
2014-03-13 08:12:42 +0100

    New plugin added: Rootsweb.

---
+++ plugins/rootsweb.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class rootsweb_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Rootsweb';
+	}
+
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 'http://worldconnect.rootsweb.ancestry.com/cgi-bin/igm.cgi?op=Search&lang=en&surname='.str_replace(" ", "+", $surname).'&stype=Exact&given='.str_replace(" ", "+", $givn).'&brange=0&drange=0&mrange=0&period=All&submit.x=Search';
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+}
