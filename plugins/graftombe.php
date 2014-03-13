ad7e068c8d5cabeafb891b369f72a1bd583370db
JustCarmen <carmen@justcarmen.nl>
2014-03-13 22:17:31 +0100

    New plugin added: Graftombe (http://www.graftombe.nl)

---
+++ plugins/graftombe.php
@@ -0,0 +1,20 @@
+<?php
+
+if (!defined('WT_WEBTREES')) {
+	header('HTTP/1.0 403 Forbidden');
+	exit;
+}
+
+class graftombe_plugin extends research_base_plugin {
+	static function getName() {
+		return 'Graftombe';
+	}
+
+	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return $link = 'http://www.graftombe.nl/names/search?forename='.str_replace(" ", "+", $givn).'&surname='.str_replace(" ", "+", $surname).'&submit=Zoeken&r=names-search';
+	}
+
+	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
+		return false;
+	}
+}
