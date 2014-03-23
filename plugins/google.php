42c92bf907f2b6a204c4cbcef5b260e177ae45d4
JustCarmen <carmen@justcarmen.nl>
2014-03-23 08:53:37 +0100

    Link google.nl changed to google.com.

--- plugins/google.php
+++ plugins/google.php
@@ -11,7 +11,7 @@ class google_plugin extends research_base_plugin {
 	}	
 	
 	static function create_link($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
-		return $link = 'https://www.google.nl/search?q='.str_replace(" ", "+", $fullname);
+		return $link = 'https://www.google.com/search?q='.str_replace(" ", "+", $fullname);
 	}
 
 	static function create_sublink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
