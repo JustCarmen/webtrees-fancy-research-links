b3fc54420ee68cc0a947605454a60e62f55da522
JustCarmen <carmen@justcarmen.nl>
2015-04-25 09:25:26 +0200

    Narrow the Google search.

--- plugins/int_google.php
+++ plugins/int_google.php
@@ -24,7 +24,7 @@ class int_google_plugin extends research_base_plugin {
 	}
 
 	static function createLink($fullname, $givn, $first, $middle, $prefix, $surn, $surname) {
-		return $link = 'https://www.google.com/search?q=' . $fullname;
+		return $link = 'https://www.google.com/search?q="' . $fullname . '"';
 	}
 
 	static function encode_plus() {
