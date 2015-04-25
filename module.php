7babbabcf9aceabfdc116fceadfb5ad4b43f0a33
JustCarmen <carmen@justcarmen.nl>
2015-04-25 20:36:31 +0200

    Unused use statement.

--- module.php
+++ module.php
@@ -17,8 +17,6 @@ namespace Fisharebest\Webtrees;
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use Zend_Translate;
-
 class FancyResearchLinksModule extends AbstractModule implements ModuleConfigInterface, ModuleSidebarInterface {
 
 	public function __construct() {
