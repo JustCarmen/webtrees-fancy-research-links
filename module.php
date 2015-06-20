ee704017400182ec3dfbbf415431bcc3f03e0927
JustCarmen <carmen@justcarmen.nl>
2015-06-21 00:35:11 +0200

    seperate AdminTemplate.

--- module.php
+++ module.php
@@ -17,16 +17,14 @@
 namespace JustCarmen\WebtreesAddOns\Module\FancyResearchLinks;
 
 use Composer\Autoload\ClassLoader;
-use Fisharebest\Webtrees\Auth;
-use Fisharebest\Webtrees\Controller\PageController;
 use Fisharebest\Webtrees\Database;
 use Fisharebest\Webtrees\Filter;
-use Fisharebest\Webtrees\Functions\FunctionsEdit;
 use Fisharebest\Webtrees\I18N;
 use Fisharebest\Webtrees\Log;
 use Fisharebest\Webtrees\Module\AbstractModule;
 use Fisharebest\Webtrees\Module\ModuleConfigInterface;
 use Fisharebest\Webtrees\Module\ModuleSidebarInterface;
+use JustCarmen\WebtreesAddOns\Module\FancyResearchLinks\Template\AdminTemplate;
 
 class FancyResearchLinksModule extends AbstractModule implements ModuleConfigInterface, ModuleSidebarInterface {
 
@@ -60,11 +58,16 @@ class FancyResearchLinksModule extends AbstractModule implements ModuleConfigInt
 	public function modAction($mod_action) {
 		switch ($mod_action) {
 			case 'admin_config':
-				$this->config();
-				break;
+				if (Filter::postBool('save')) {
+					$this->setSetting('FRL_PLUGINS', serialize(Filter::post('NEW_FRL_PLUGINS')));
+					Log::addConfigurationLog($this->getTitle() . ' config updated');
+				}
+				$template = new AdminTemplate;
+				return $template->pageContent();
 			case 'admin_reset':
-				$this->resetAll();
-				$this->config();
+				Database::prepare("DELETE FROM `##module_setting` WHERE setting_name LIKE 'FRL%'")->execute();
+				Log::addConfigurationLog($this->getTitle() . ' reset to default values');
+				header('Location: ' . $this->getConfigLink());
 				break;
 			default:
 				http_response_code(404);
@@ -74,86 +77,7 @@ class FancyResearchLinksModule extends AbstractModule implements ModuleConfigInt
 
 	// Implement WT_Module_Config
 	public function getConfigLink() {
-		return 'module.php?mod=' . $this->getName() . '&amp;mod_action=admin_config';
-	}
-
-	// Reset all settings to default
-	private function resetAll() {
-		Database::prepare("DELETE FROM `##module_setting` WHERE setting_name LIKE 'FRL%'")->execute();
-		Log::addConfigurationLog($this->getTitle() . ' reset to default values');
-	}
-
-	// Configuration page
-	private function config() {
-		$controller = new PageController;
-		$controller
-			->restrictAccess(Auth::isAdmin())
-			->setPageTitle(I18N::translate('Fancy Research Links'))
-			->pageHeader()
-			->addInlineJavascript('
-				jQuery("head").append("<style>[dir=rtl] .checkbox-inline input[type=checkbox]{margin-left:-20px}</style>");
-				jQuery("input[name=select-all]").click(function(){
-					if (jQuery(this).is(":checked") == true) {
-						jQuery("#linklist").find(":checkbox").prop("checked", true).val(1);
-						jQuery("input[id^=NEW_FRL_PLUGINS]").val(1);
-					} else {
-						jQuery("#linklist").find(":checkbox").prop("checked", false).val(0);
-						jQuery("input[id^=NEW_FRL_PLUGINS]").val(0);
-					}
-					formChanged = true;
-				});
-			');
-
-		if (Filter::postBool('save')) {
-			$this->setSetting('FRL_PLUGINS', serialize(Filter::post('NEW_FRL_PLUGINS')));
-			Log::addConfigurationLog($this->getTitle() . ' config updated');
-		}
-
-		$FRL_PLUGINS = unserialize($this->getSetting('FRL_PLUGINS'));
-		?>
-		<ol class="breadcrumb small">
-			<li><a href="admin.php"><?php echo I18N::translate('Control panel'); ?></a></li>
-			<li><a href="admin_modules.php"><?php echo I18N::translate('Module administration'); ?></a></li>
-			<li class="active"><?php echo $controller->getPageTitle(); ?></li>
-		</ol>
-		<h2><?php echo $controller->getPageTitle(); ?></h2>
-		<p class="small text-muted"><?php echo I18N::translate('Check the plugins you want to use in the sidebar'); ?></p>
-		<form class="form-horizontal" method="post" name="configform" action="<?php echo $this->getConfigLink(); ?>">
-			<input type="hidden" name="save" value="1">
-			<!-- SELECT ALL -->
-			<div class="checkbox-inline">
-				<label>
-					<?php echo FunctionsEdit::checkbox('select-all') . I18N::translate('select all'); ?>
-				</label>
-			</div>
-			<!-- RESEARCH LINKS -->
-			<div id="linklist" class="form-group">
-				<?php foreach (FancyResearchLinksClass::getPluginList() as $plugin_label => $plugin): ?>
-					<?php
-					if (is_array($FRL_PLUGINS) && array_key_exists($plugin_label, $FRL_PLUGINS)) {
-						$value = $FRL_PLUGINS[$plugin_label];
-					} if (!isset($value)) {
-						$value = '1';
-					}
-					?>
-					<div class="checkbox col-xs-4" dir="ltr">
-						<label class="checkbox-inline">
-							<?php echo FunctionsEdit::twoStateCheckbox('NEW_FRL_PLUGINS[' . $plugin_label . ']', $value) . ' ' . $plugin->getPluginName(); ?>
-						</label>
-					</div>
-				<?php endforeach; ?>
-			</div>
-			<button type="submit" class="btn btn-primary">
-				<i class="fa fa-check"></i>
-				<?php echo I18N::translate('Save'); ?>
-			</button>
-			<button type="reset" class="btn btn-primary" onclick="if (confirm('<?php echo I18N::translate('The settings will be reset to default. Are you sure you want to do this?'); ?>'))
-						window.location.href = 'module.php?mod=<?php echo $this->getName(); ?>&amp;mod_action=admin_reset';">
-				<i class="fa fa-recycle"></i>
-				<?php echo I18N::translate('Reset'); ?>
-			</button>
-		</form>
-		<?php
+		return 'module.php?mod=' . $this->getName() . '&mod_action=admin_config';
 	}
 
 	// Implement WT_Module_Sidebar
