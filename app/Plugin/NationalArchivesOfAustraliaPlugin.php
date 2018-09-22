4f8e08f7ce4b05b55f4fe0c394b7106e71c98b11
JustCarmen <carmen@justcarmen.nl>
2018-09-22 11:01:47 +0200

    Two new Australian research sites.

---
+++ app/Plugin/NationalArchivesOfAustraliaPlugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2017 webtrees development team
+ * Copyright (C) 2017 JustCarmen
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace JustCarmen\WebtreesAddOns\FancyResearchLinks\Plugin;
+
+use JustCarmen\WebtreesAddOns\FancyResearchLinks\FancyResearchLinksClass;
+
+class NationalArchivesOfAustraliaPlugin extends FancyResearchLinksClass {
+
+	static function getPluginName() {
+		return 'National Archives of Australia';
+	}
+
+	static function getSearchArea() {
+		return 'AUS';
+	}
+
+	static function createLink($name) {
+		return "https://recordsearch.naa.gov.au";
+	}
+
+}
