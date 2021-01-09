b1e66cc83232d67cf26324457d9ecd18f7f89868
JustCarmen <carmen@justcarmen.nl>
2021-01-09 10:37:34 +0100

    Setup the new environment for webtrees 2

---
+++ FancyResearchLinksModule.php
@@ -0,0 +1,185 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks;
+
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\View;
+use Psr\Http\Message\ResponseInterface;
+use Fisharebest\Localization\Translation;
+use Psr\Http\Message\ServerRequestInterface;
+use Fisharebest\Webtrees\Services\TreeService;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleConfigTrait;
+use Fisharebest\Webtrees\Module\ModuleCustomTrait;
+use Fisharebest\Webtrees\Module\ModuleConfigInterface;
+use Fisharebest\Webtrees\Module\ModuleCustomInterface;
+
+class FancyResearchLinksModule extends AbstractModule implements ModuleCustomInterface, ModuleConfigInterface
+{
+    use ModuleCustomTrait;
+    use ModuleConfigTrait;
+
+    /** @var TreeService */
+    private $tree_service;
+
+    /**
+     * FancyResearchLinks constructor.
+     *
+     * @param TreeService       $tree_service
+     */
+    public function __construct(TreeService $tree_service)
+    {
+        $this->tree_service = $tree_service;
+    }
+
+    /**
+     * How should this module be identified in the control panel, etc.?
+     *
+     * @return string
+     */
+    public function title(): string
+    {
+        return I18N::translate('Fancy Research Links');
+    }
+
+    /**
+     * A sentence describing what this module does.
+     *
+     * @return string
+     */
+    public function description(): string
+    {
+        return I18N::translate('A sidebar tool to provide quick links to popular research web sites.');
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleAuthorName()
+     */
+    public function customModuleAuthorName(): string
+    {
+        return 'JustCarmen';
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleVersion()
+     *
+     * We use a system where the version number is equal to the latest version of webtrees
+     * Interim versions get an extra sub number
+     *
+     * The dev version is always one step above the latest stable version of this module
+     * The subsequent stable version depends on the version number of the latest stable version of webtrees
+     *
+     */
+    public function customModuleVersion(): string
+    {
+        return '2.0.11-dev';
+    }
+
+    /**
+     * A URL that will provide the latest stable version of this module.
+     *
+     * @return string
+     */
+    public function customModuleLatestVersionUrl(): string
+    {
+        return 'https://raw.githubusercontent.com/JustCarmen/webtrees-fancy-research-links/master/latest-version.txt';
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see \Fisharebest\Webtrees\Module\ModuleCustomInterface::customModuleSupportUrl()
+     */
+    public function customModuleSupportUrl(): string
+    {
+        return 'https://github.com/justcarmen/webtrees-fancy-research-links/issues';
+    }
+
+    /**
+     * Bootstrap.  This function is called on *enabled* modules.
+     * It is a good place to register routes and views.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        // Register a namespace for our views.
+        View::registerNamespace($this->name(), $this->resourcesFolder() . 'views/');
+    }
+
+    /**
+     * Where does this module store its resources
+     *
+     * @return string
+     */
+    public function resourcesFolder(): string
+    {
+        return __DIR__ . '/resources/';
+    }
+
+    /**
+     * Where does this module store its plugins
+     *
+     * @return string
+     */
+    public function pluginsFolder(): string
+    {
+        return $this->resourcesFolder() . 'plugins/';
+    }
+
+    /**
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function getAdminAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $this->layout = 'layouts/administration';
+
+        return $this->viewResponse($this->name() . '::settings', [
+
+        ]);
+    }
+
+    /**
+     * Save the user preference.
+     *
+     * @param ServerRequestInterface $request
+     *
+     * @return ResponseInterface
+     */
+    public function postAdminAction(ServerRequestInterface $request): ResponseInterface
+    {
+        $params = (array) $request->getParsedBody();
+
+        // store the preferences in the database
+        $all_trees = $this->tree_service->all();
+
+        if ($params['save'] === '1') {
+
+        }
+
+        return redirect($this->getConfigLink());
+    }
+
+    /**
+     * Additional/updated translations.
+     *
+     * @param string $language
+     *
+     * @return string[]
+     */
+    public function customTranslations(string $language): array
+    {
+        $lang_dir   = $this->resourcesFolder() . 'lang/';
+        $file       = $lang_dir . $language . '.mo';
+        if (file_exists($file)) {
+            return (new Translation($file))->asArray();
+        } else {
+            return [];
+        }
+    }
+};
---
+++ module-oud.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ * webtrees: online genealogy
+ * Copyright (C) 2018 JustCarmen (http://justcarmen.nl)
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace JustCarmen\WebtreesAddOns\FancyResearchLinks;
+
+use Composer\Autoload\ClassLoader;
+use Fisharebest\Webtrees\Controller\BaseController;
+use Fisharebest\Webtrees\Database;
+use Fisharebest\Webtrees\Filter;
+use Fisharebest\Webtrees\Html;
+use Fisharebest\Webtrees\I18N;
+use Fisharebest\Webtrees\Individual;
+use Fisharebest\Webtrees\Log;
+use Fisharebest\Webtrees\Module\AbstractModule;
+use Fisharebest\Webtrees\Module\ModuleConfigInterface;
+use Fisharebest\Webtrees\Module\ModuleMenuInterface;
+use Fisharebest\Webtrees\Module\ModuleSidebarInterface;
+use Fisharebest\Webtrees\Tree;
+use JustCarmen\WebtreesAddOns\FancyResearchLinks\Template\AdminTemplate;
+
+class FancyResearchLinksModule extends AbstractModule implements ModuleConfigInterface, ModuleSidebarInterface, ModuleMenuInterface {
+	const CUSTOM_VERSION = '2.0.0-dev';
+	const CUSTOM_WEBSITE = 'http://www.justcarmen.nl/fancy-modules/fancy-research-links/';
+
+	/** @var array primary name */
+	public $primary;
+
+	/**
+	 * @var array extra attributes to use in search queries
+	 * attribute must be a string
+	 */
+	public $attrs;
+
+	/** @var string location of the module files */
+	public $directory;
+
+	public function __construct() {
+		parent::__construct('fancy_research_links');
+
+		$this->directory = WT_MODULES_DIR . $this->getName();
+
+		// register the namespace
+		$loader = new ClassLoader();
+		$loader->addPsr4('JustCarmen\\WebtreesAddOns\\FancyResearchLinks\\', WT_MODULES_DIR . $this->getName() . '/app');
+		$loader->register();
+	}
+
+	/**
+	 * Get the module class.
+	 *
+	 * Class functions are called with $this inside the source directory.
+	 */
+	private function module() {
+		return new FancyResearchLinksClass;
+	}
+
+	// Extend WT_Module
+	public function getTitle(): string {
+		return /* I18N: Name of the module */ I18N::translate('Fancy Research Links');
+	}
+
+	public function getSidebarTitle() {
+		return /* Title used in the sidebar */ I18N::translate('Research links');
+	}
+
+	// Extend WT_Module
+	public function getDescription(): string {
+		return /* I18N: Description of the module */ I18N::translate('A sidebar tool to provide quick links to popular research web sites.');
+	}
+
+	// Extend WT_Module_Config
+	public function modAction($mod_action) {
+		switch ($mod_action) {
+	  case 'admin_config':
+		if (Filter::post('action') === 'save' && Filter::checkCsrf()) {
+			$this->setPreference('FRL_PLUGINS', implode(',', Filter::postArray('NEW_FRL_PLUGINS')));
+			$this->setPreference('FRL_DEFAULT_AREA', Filter::post('FRL_DEFAULT_AREA'));
+			$this->setPreference('FRL_TARGET_BLANK', Filter::post('FRL_TARGET_BLANK'));
+			Log::addConfigurationLog($this->getTitle() . ' config updated');
+		}
+		$template = new AdminTemplate;
+		return $template->pageContent();
+	  case 'admin_reset':
+		Database::prepare("DELETE FROM `##module_setting` WHERE setting_name LIKE 'FRL%'")->execute();
+		Log::addConfigurationLog($this->getTitle() . ' reset to default values');
+		header('Location: ' . $this->getConfigLink());
+		break;
+	  default:
+		http_response_code(404);
+		break;
+	}
+	}
+
+	/** {@inheritdoc} */
+	public function getConfigLink(): string {
+		return Html::url('module.php', [
+			'mod'        => $this->getName(),
+			'mod_action' => 'admin_config',
+		]);
+	}
+
+	// Implement WT_Module_Sidebar
+	public function defaultSidebarOrder(): int {
+		return 9;
+	}
+
+	// Implement WT_Module_Sidebar
+	public function hasSidebarContent(Individual $individual): bool {
+		return true;
+	}
+
+	// Implement WT_Module_Sidebar
+	public function getSidebarAjaxContent() {
+		return false;
+	}
+
+	// Implement WT_Module_Sidebar
+	public function getSidebarContent(Individual $individual): string {
+		// code based on similar in function_print_list.php
+		global $controller, $WT_TREE;
+
+		$controller->addInlineJavascript('
+			$("#sidebar-header-' . $this->getName() . ' a").text("' . $this->getSidebarTitle() . '");
+		', BaseController::JS_PRIORITY_HIGH);
+
+		$controller->addInlineJavascript('
+
+      $(".fancy-research-links").each(function() {
+        // expand the default search area
+        $(this).find(".frl-area").each(function(){
+          if ($(this).data("area") === "' . $this->getPreference('FRL_DEFAULT_AREA') . '") {
+            $(this).find(".frl-list").css("display", "block");
+          }
+        });
+
+        $(this).on("click", ".frl-area-title", function(e){
+          e.preventDefault();
+          $(this).next(".frl-list").slideToggle()
+          $(this).parent().siblings().find(".frl-list").slideUp();
+          $("a[rel^=external]").attr("target", "_blank");
+        });
+       });
+				
+			// function for use by research links which need a javascript form submit
+			// source: see http://stackoverflow.com/questions/133925/javascript-post-request-like-a-form-submit
+			// usage: see OnlineBegraafplaatsen plugin and MetaGenealogy plugin
+			function postresearchform(url, params) {
+				
+				var form = document.createElement("form");
+				form.setAttribute("method", "post");
+				form.setAttribute("action", url);
+				form.setAttribute("target", "_blank");
+				
+				for(var key in params) {
+					if(params.hasOwnProperty(key)) {
+						var hiddenField = document.createElement("input");
+						hiddenField.setAttribute("type", "hidden");
+						hiddenField.setAttribute("name", key);
+						hiddenField.setAttribute("value", params[key]);
+						
+						form.appendChild(hiddenField);
+					 }
+				}
+				
+				document.body.appendChild(form);
+				form.submit();
+			}
+		');
+
+		try {
+			$html = '<div class="fancy-research-links">';
+			$html .= '<ul class="fa-ul">';
+
+			$i                     = 0;
+			$total_enabled_plugins = 0;
+
+			$record = Individual::getInstance(Filter::get('xref'), $WT_TREE, Filter::get('ged'));
+
+			foreach ($this->module()->getPluginList() as $area => $plugins) {
+				$FRL_PLUGINS = $this->module()->getEnabledPlugins($plugins);
+
+				// count enabled plugins in this area
+				$count_enabled_plugins = $this->module()->countEnabledPlugins($plugins, $FRL_PLUGINS);
+
+				// count enabled plugins in all areas
+				$total_enabled_plugins = $total_enabled_plugins + $count_enabled_plugins;
+
+				if ($count_enabled_plugins > 0) {
+					// reset returns the first value in an array
+					// we take the area code from the first plugin in this area
+					$area_code = reset($plugins)->getSearchArea();
+					$html .= '<li class="frl-area" data-area="' . $area_code . '">' .
+			  '<i class="fa-li fa fa-caret-right"></i>' .
+			  '<a href="#" class="frl-area-title h5">' . $area . ' (' . $count_enabled_plugins . ')' . '</a>' .
+			  '<ul class="frl-list fa-ul">';
+					$i++;
+					foreach ($plugins as $label => $plugin) {
+						if (in_array($label, $FRL_PLUGINS)) {
+							foreach ($record->getFacts() as $fact) {
+								$tag = $fact->getTag();
+								if ($tag == "NAME") {
+									$this->primary = $this->module()->getPrimaryName($fact);
+									break; // only use the first fact with a NAME tag found.
+								}
+							}
+
+							if ($this->primary) {
+								$this->attrs = [
+									'birthyear'  => $record->getBirthYear(),
+									'birthplace' => $record->getBirthPlace(),
+									'deathyear'  => $record->getDeathYear(),
+									'deathplace' => $record->getDeathPlace()
+								];
+								$link = $plugin->createLink($this->module()->getNames($this->primary, $this->attrs, $plugin->encodePlus()));
+								if ($this->getSetting('FRL_TARGET_BLANK') === '1') {
+									$target = 'target="_blank"';
+								} else {
+									$target = '';
+								}
+								$html .= '<li><i class="fa-li fa fa-external-link"></i>' .
+								'<a href="' . Html::escape($link) . '" ' . $target . ' rel="external nofollow">' .
+								$plugin->getPluginName() .
+								'</a>' .
+								'</li>';
+							}
+						}
+					}
+					$html .= '</ul>';
+				}
+			}
+			$html .= '</ul>';
+
+			if ($total_enabled_plugins === 0) {
+				$html = I18N::translate('There are no research links available for this individual.');
+			}
+			$html .= '</div>';
+
+			return $html;
+		} catch (ErrorException $ex) {
+			Log::addErrorLog('Fancy ResearchLinks: ' . $ex->getMessage());
+			return I18N::translate('There are no research links available for this individual.');
+		}
+	}
+
+	// Implement ModuleMenuInterface
+	public function defaultMenuOrder(): int {
+		return 999;
+	}
+
+	// Implement ModuleMenuInterface
+	public function getMenu(Tree $tree) {
+		// We don't actually have a menu - this is just a convenient "hook" to execute code at the right time during page execution
+		// We need it to load the stylesheet at the right time. If we use this method in the sidebar funcion the stylesheet loads before
+		// the other stylesheets. This might give problems.
+
+		if (WT_SCRIPT_NAME === 'individual.php') {
+			echo $this->includeCss();
+		}
+		return null;
+	}
+
+	/**
+	 * Default Fancy script to load a module stylesheet
+	 *
+	 * The code to place the stylesheet in the header renders quicker than the default webtrees solution
+	 * because we do not have to wait until the page is fully loaded
+	 *
+	 * @return javascript
+	 */
+	protected function includeCss() {
+		return
+		'<script>
+				var newSheet=document.createElement("link");
+				newSheet.setAttribute("rel","stylesheet");
+				newSheet.setAttribute("type","text/css");
+				newSheet.setAttribute("href","' . $this->directory . '/css/style.css");				
+				document.getElementsByTagName("head")[0].appendChild(newSheet);
+			</script>';
+	}
+}
+
+return new FancyResearchLinksModule;
