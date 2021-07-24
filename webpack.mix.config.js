4095bc3e76b7bd59cbfcaa4ba6f0bffb20633b11
JustCarmen <carmen@justcarmen.nl>
2021-07-24 12:54:49 +0200

    Include the plugins folder in the distribution package

--- webpack.mix.config.js
+++ webpack.mix.config.js
@@ -5,5 +5,6 @@
 module.exports = {
     version: '2.0.0',
     public_dir: 'resources',
+    plugins_dir: 'plugins',
     dist_dir: 'dist/jc-fancy-research-links'
 }
--- webpack.mix.distribution.js
+++ webpack.mix.distribution.js
@@ -20,6 +20,7 @@ require('laravel-mix-clean');
 
 mix
     .setPublicPath('./dist')
+    .copyDirectory(config.plugins_dir, config.dist_dir + '/plugins')
     .copyDirectory(config.public_dir + '/views', config.dist_dir + '/resources/views')
     .copy(config.public_dir + '/lang/*.mo', config.dist_dir + '/resources/lang')
     .copy('FancyResearchLinksModule.php', config.dist_dir)
