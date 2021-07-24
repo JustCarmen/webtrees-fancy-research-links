20b5e22b722d41cb24e7ff838a0ca6003c096a60
Yuri <zaharchenko.yurij@gmail.com>
2021-07-24 18:03:23 +0300

    reabit.org.ua
    Search the database of repressed persons

---
+++ plugins/ReabitPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class ReabitPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'Національний банк репресованих'; /*National Bank of Repressed Persons*/
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'UKR';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'http://www.reabit.org.ua/nbr/?st=4&region=&ss=&logic=or&f1_type=begins&f1_str=' . $name['surn'] . '&f2_type=begins&f2_str=' . $name['givn'];
+    }
+}
