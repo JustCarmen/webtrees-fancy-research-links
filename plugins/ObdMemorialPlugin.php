db77d4becea14b48faea4f5168027ab74a46df57
Yuri <zaharchenko.yurij@gmail.com>
2021-07-24 18:57:42 +0300

    obd-memorial.ru
    Dead soldiers of the Soviet Union in the World War II

---
+++ plugins/ObdMemorialPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class ObdMemorialPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'ОБД Мемориал';
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'RUS';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'https://obd-memorial.ru/html/search.htm?f=' . $name['surn'] . '&n=' . $name['givn'] . '&s=&y=&r=';
+    }
+}
