8b5dd9e8c229f255a93c94d65ff1f3a69dc90cfa
JustCarmen <carmen@justcarmen.nl>
2021-07-26 21:01:14 +0200

    Close #27 - New plugin: Chronicling America

---
+++ plugins/ChroniclingAmericaPlugin.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class ChroniclingAmericaPlugin extends FancyResearchLinksModule
+{
+	public static function pluginLabel(): string
+    {
+		return 'Chronicling America';
+	}
+
+	public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public static function researchArea(): string
+    {
+		return 'USA';;
+	}
+
+	public static function researchLink($name, $birth): string
+
+    {
+		return 'https://chroniclingamerica.loc.gov/search/pages/results/?state=&date1=' . $birth['year'] . '&date2=1963&proxtext=' . $name['first'] . '+' . $name['surname'] . '&x=23&y=17&dateFilterType=yearRange&rows=20&searchType=basic&sort=date';
+	}
+}
