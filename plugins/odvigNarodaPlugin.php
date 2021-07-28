8a532f6f9c652f699d7e469fb026cf1b21a849b6
Yuri <zaharchenko.yurij@gmail.com>
2021-07-28 15:47:19 +0300

    Create odvigNarodaPlugin.php
    Bank of documents "The feat of the people in the Great Patriotic War of 1941-1945"

---
+++ plugins/odvigNarodaPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class PodvigNarodaPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'Банк документов "Подвиг народа (ссылка)"';
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'RUS';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'http://podvignaroda.ru/?#tab=navPeople_search';
+    }
+}
