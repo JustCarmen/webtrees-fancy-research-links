93577f357606baa60a6821e0f417afc3252ab4f6
Yuri <zaharchenko.yurij@gmail.com>
2021-07-28 12:44:48 +0300

    Create UkrainianLibertationMovementPlugin.php

---
+++ plugins/UkrainianLibertationMovementPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class UkrainianLibertationMovementPluginPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'Архів Українського визвольного руху (посилання)';
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'UKR';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'http://avr.org.ua/';
+    }
+}
