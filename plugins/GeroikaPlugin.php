189c5a6514682df11beeb3a59ebca971cc7af6a0
Yuri <zaharchenko.yurij@gmail.com>
2021-07-28 15:24:18 +0300

    Create GeroikaPlugin.php
    Soldiers of the Central Rada, the Army of the Ukrainian State, the troops of the Directory, the Army of the Ukrainian People's Republic and the Galician Army

---
+++ plugins/GeroikaPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class GeroikaPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'Пошук в базі даних військових УНР (посилання)';
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'UKR';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'http://www.db.geroika.org.ua/ua/search.html';
+    }
+}
