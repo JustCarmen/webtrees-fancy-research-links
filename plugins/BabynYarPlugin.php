5bd597aacb9e9a905cc7cda390f1d816e1fa3552
Yuri <zaharchenko.yurij@gmail.com>
2021-07-28 15:29:54 +0300

    Create BabynYarPlugin.php
    Search among those shot in Babyn Yar

---
+++ plugins/BabynYarPlugin.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class BabynYarPlugin extends FancyResearchLinksModule
+{
+    public static function pluginLabel(): string
+    {
+        return 'Cписок убитих в Бабиному Яру (посилання)';
+    }
+
+    public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public static function researchArea(): string
+    {
+        return 'UKR';
+    }
+
+    public static function researchLink($name): string
+    {
+        return 'https://babynyar.org/ua/names';
+    }
+}
