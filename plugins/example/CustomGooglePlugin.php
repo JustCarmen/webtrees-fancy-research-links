93babd78455143cc5bea7ae18b3f35f61598940d
JustCarmen <carmen@justcarmen.nl>
2021-07-31 08:01:07 +0200

    Update example

--- plugins/example/CustomGooglePlugin.php
+++ plugins/example/CustomGooglePlugin.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
 
-use Fisharebest\Webtrees\I18N;
 use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
 
 class CustomGooglePlugin extends FancyResearchLinksModule
@@ -17,7 +16,12 @@ class CustomGooglePlugin extends FancyResearchLinksModule
    */
 	public static function pluginLabel(): string
     {
-		return 'Google custom 1';
+		return 'Google custom search';
+	}
+
+	public static function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
 	}
 
 	/**
@@ -43,52 +47,46 @@ class CustomGooglePlugin extends FancyResearchLinksModule
 	 * - Last name without prefix = $name[‘surn’] e.g. "Burgh"
 	 * - Prefix = $name[‘prefix’] e.g. "van den"
 	 * - Birth year = $birth[‘year’] e.g. "1800"
-	 * - Birth place = $birth[‘place’] e.g. "Palos Heights, Cook County, Illinois, USA"
+	 * - Birth place = $birth[‘place’] e.g. "Chigaco"
 	 * - Death year = $death[‘year’] e.g. "1880"
-	 * - Death place = $death[‘place’] e.g. "Palos Heights, Cook County, Illinois, USA"
+	 * - Death place = $death[‘place’] e.g. "Chigaco"
 	 *
 	 * @return string
 	 */
 	public static function researchLink($name, $birth, $death): string
     {
 		// "First M Last" YOB
-		$fullNN = $name['first'];
+		$searchname = $name['first'];
 
 		// Extend the firstname with the first letter of the middle name.
-		// Extract the substring from position 0 with a length of 1
-		// With .= means concatenating the string with the string in the already declared variable $fullNN
-		if ($name['middle']) {
-			$fullNN .= ' ' . substr($name['middle'], 0, 1);
+		// First seperate the first name from the second part of the given name
+		// Extract the substring (the first letter of the middlename) from position 0 with a length of 1
+		// With .= means concatenating the string with the string in the already declared variable $searchname
+		$middlename = trim(str_replace($name['first'], '', $name['givn']));
+		$searchname .= ' ' . substr($middlename, 0, 1);
+
+		// Concatenate the surname to the variable $searchname that already holds the string "First M"
+		$searchname .= ' ' . $name['surname'];
+
+		// The variable birth['year']/birth['place] (or death['year']/death['place']) returns an empty string when the date/place
+		// is unknown so it is not neccessary to use an escape, but if the string is empty we do not need the preceding space.
+		// This might not be a problem in a googlesearch but it can be a problem with other research websites.
+		// In this case it is better to add the space to the variable. You can replace $birth with $death in this example.
+		$year = $birth['year'];
+		if ($year <> '') { // if $year is not empty
+			$year = ' ' . $year;
+		}
+		$place = $birth['place'];
+		if ($place <> '') { // if $place is not empty
+			$place = ' ' . $place;
 		}
 
-		// Concatenate the surname to the variable $fullNN that already holds the string "First M"
-		$fullNN .= ' ' . $name['surname'];
-
-		// Use an escape in case the birthyear is empty
-		if ($birth['year']) {
-			$birthyear = ' ' . $birth['year'];
-		} else {
-			$birthyear = '';
-		}
-
-		// Use the full birthplace or a short version.
-		// [0] is the first part of the full placename, [1] is the second part etc.
-		// Use an escape in case the deathplace is emtpy
-		if ($birth['place']) {
-			// $birthplace is the full placename e.g. "Palos Heights, Cook County, Illinois, USA"
-			$birthplace = ' ' . $name['birthplace'];
-			// $shortbirthplace is a part of the placename e.g. "Palos Heights"
-			$shortbirthplace = ' ' . explode(", ", $death['place'])[0];
-		} else {
-			$birthplace      = '';
-			$shortbirthplace = '';
-		}
-
-		// narrow down the search by using ~genealogy and ~ancestry. Google will only return related searches.
-		// put the $fullNN variable between double quotes to search the whole name only. Remove the double quotes to search
-		// each name part separatly. Maybe in that case don't use the first letter of the middle name (see above).
-		// The single quotes are used to escape the variables in the string.
+		// Narrow the search by using ~genealogy and/or ~ancestry. Google will return only related searches.
+		// Enclose the variable $searchname in double quotes to search only the entire name. Remove the double quotes to search
+		// each name part separately. In this case, it may be better to exclude the first letter of the middle name (see above).
+		// The single quotes are used to escape the variables in the string. Use spaces in the string where appropriate.
 		// The dots are used to concatenate the variables with the previous part.
-		return 'https://www.google.com/search?q="' . $fullNN . '"' . $birthyear . $birthplace . ' ~genealogy ~ancestry';
+		// Of course, birth year/place of birth and death year/place of death are replaceable in this example.
+		return 'https://www.google.com/search?q="' . $searchname . '"' . $year . $place . ' ~genealogy ~ancestry';
 	}
 }
