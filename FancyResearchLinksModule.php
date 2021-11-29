c4207eb5f670cae2a5ba6442d0f4b527894d02a7
JustCarmen <carmen@justcarmen.nl>
2021-11-29 17:11:40 +0100

    Replace deprecated functions

--- FancyResearchLinksModule.php
+++ FancyResearchLinksModule.php
@@ -229,9 +229,9 @@ class FancyResearchLinksModule extends AbstractModule implements ModuleCustomInt
         $name['first']      = explode(" ", $name['givn'])[0];
         $name['prefix']     = trim(str_replace($name['surn'], '', $name['surname']));
         $name['fullNN']     = trim(strip_tags(str_replace('@N.N.', '', $name['fullNN'])));
-        $birth['year']      = $individual->getBirthYear();
+        $birth['year']      = $individual->getBirthDate()->minimumDate()->format('%Y');
         $birth['place']     = strip_tags(str_replace(I18N::translate('unknown'), '', $individual->getBirthPlace()->placeName()));
-        $death['year']      = $individual->getDeathYear();
+        $death['year']      = $individual->getDeathDate()->minimumDate()->format('%Y');
         $death['place']     = strip_tags(str_replace(I18N::translate('unknown'), '', $individual->getDeathPlace()->placeName()));
 
         $expand_sidebar     = (bool) $this->getPreference('expand-sidebar') && Auth::isEditor($individual->tree());
