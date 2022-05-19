4082f5a5599b83ad9ea37cbd04b3e1ee9a5f1524
JustCarmen <carmen@justcarmen.nl>
2022-05-19 20:46:22 +0200

    Close #59 Narrow the searchresults at FamilySearch

--- plugins/FamilySearchPlugin.php
+++ plugins/FamilySearchPlugin.php
@@ -9,25 +9,41 @@ use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
 
 class FamilySearchPlugin extends FancyResearchLinksModule
 {
-	public function pluginLabel(): string
+    public function pluginLabel(): string
     {
-		return 'Family Search';
-	}
+        return 'FamilySearch';
+    }
 
-	public function pluginName(): string
-	{
-		return strtolower(basename(__FILE__, 'Plugin.php'));
-	}
-
-	public function researchArea(): string
+    public function pluginName(): string
     {
-		return 'INT';
-	}
+        return strtolower(basename(__FILE__, 'Plugin.php'));
+    }
 
-	public function researchLink($attributes): string
+    public function researchArea(): string
     {
-		$name = $attributes['NAME'];
+        return 'INT';
+    }
 
-		return 'https://www.familysearch.org/search/record/results?q.givenName=' . $name['givn'] . '&q.surname=' . $name['surname'];
-	}
+    public function researchLink($attributes): string
+    {
+        $name = $attributes['NAME'];
+        $year = $attributes['YEAR'];
+        $plac = $attributes['PLACE'];
+
+        $bf = ((int)$year['BIRT']) < 1000 ? '' : (int)$year['BIRT']-1;
+        $bt = ((int)$year['BIRT']) < 1000 ? '' : (int)$year['BIRT']+1;
+        $df = ((int)$year['DEAT']) < 1000 ? '' : (int)$year['DEAT']-1;
+        $dt = ((int)$year['DEAT']) < 1000 ? '' : (int)$year['DEAT']+1;
+
+        return 'https://www.familysearch.org/search/record/results' .
+           '?q.givenName='    		. $name['givn']    .
+           '&q.surname='      		. $name['surname'] . '%20' . $name['msurname'] .
+           '&count=100'             .
+           '&q.birthLikeDate.from=' . $bf .
+           '&q.birthLikeDate.to='   . $bt .
+           '&q.deathLikeDate.from=' . $df .
+           '&q.deathLikeDate.to='   . $dt .
+           '&q.birthLikePlace='     . $plac['BIRT']   .
+           '&q.deathLikePlace='     . $plac['DEAT'];
+    }
 }
