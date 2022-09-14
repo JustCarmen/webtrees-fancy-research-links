3100605b6cca3caf71cf269e551183882f017ed0
JustCarmen <carmen@justcarmen.nl>
2022-09-14 04:14:54 +0200

    New plugin: Regionaal Archief Dordrecht

---
+++ plugins/RegionaalArchiefDordrechtPlugin.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class RegionaalArchiefDordrechtPlugin extends FancyResearchLinksModule
+{
+  	public function pluginLabel(): string
+    {
+		return 'Regionaal Archief Dordrecht';
+	}
+
+	public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public function researchArea(): string
+    {
+		return 'NLD';
+	}
+
+	public function researchLink($attributes): string
+    {
+		$name = $attributes['NAME'];
+
+		return 'https://www.regionaalarchiefdordrecht.nl/archief/zoeken/?mivast=46&miadt=46&mizig=100&miview=tbl&milang=nl&micols=1' .
+			   '&mip1=' . $name['surn'] .
+			   '&mip2=' . $name['prefix'] .
+			   '&mip3=' . $name['givn'];
+	}
+}
