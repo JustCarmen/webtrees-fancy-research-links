dae19f2a5e8b57359b56334272b477f9c6c4659f
JustCarmen <carmen@justcarmen.nl>
2022-12-05 08:07:57 +0100

    New workflow for POEditor files

--- dist.bat
+++
@@ -1,6 +0,0 @@
-:: Compile Mo-files before distribution
-cd C:\Program Files (x86)\Poedit\GettextTools\bin
-SET SOUR=%~dp0
-FOR %%f in (%SOUR%resources\lang\*.po) DO (
-	msgfmt  %%f --output-file %%~dpf%%~nf.mo
-)
