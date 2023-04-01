600135060c0ef5fd49e33b9778036aea18c4cbd5
JustCarmen <carmen@justcarmen.nl>
2023-04-01 16:25:48 +0200

    Two new Italian plugins provided by Angelo

---
+++ plugins/CimiteroOrbassanoPlugin.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class CimiteroOrbassanoPlugin extends FancyResearchLinksModule
+{
+	public function pluginLabel(): string
+    {
+		return 'Cimitero Orbassano';
+	}
+
+	public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public static function researchArea(): string
+    {
+		return 'ITA';
+	}
+
+	public function researchLink($attributes): string
+
+    {
+		$name = $attributes['NAME'];
+		$nome = $name['first'];
+		$cognome = $name['surn'];
+
+		$place = $attributes['PLACE'];
+		$natain = $place['BIRT'];
+		$spentain = $place['DEAT'];
+		$riposain = $place['BURI'];
+
+		$anno = $attributes['YEAR'];
+		$annonascita = $anno['BIRT'];
+		$annomorte = $anno['DEAT'];
+
+		return 'https://cimitero-orbassano.geo.pa.it/?q=anagrafe-cimiteriale&field_cognome_def_value=' . $cognome
+		. '&field_nome_defunto_value=' . $nome;
+
+	}
+}
---
+++ plugins/PortaleAntenatiPlugin.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class PortaleAntenatiPlugin extends FancyResearchLinksModule
+{
+	public function pluginLabel(): string
+    {
+		return 'Portale Antenati';
+	}
+
+	public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public static function researchArea(): string
+    {
+		return 'ITA';
+	}
+
+	public function researchLink($attributes): string
+
+    {
+		$name = $attributes['NAME'];
+		$nome = $name['first'];
+		$cognome = $name['surn'];
+
+		$place = $attributes['PLACE'];
+		$natain = $place['BIRT'];
+		$spentain = $place['DEAT'];
+		$riposain = $place['BURI'];
+
+		$anno = $attributes['YEAR'];
+		$annonascita = $anno['BIRT'];
+		$annomorte = $anno['DEAT'];
+
+		return 'https://antenati.cultura.gov.it/search-nominative/?cognome=' . $cognome
+		. '&s_facet_query=nome_s%3A' . $nome
+		. '&%252Clocalita_ss%3A' . $natain
+		. '&%252CestremoRemoto_i%3A' . $annonascita;
+
+	}
+}
