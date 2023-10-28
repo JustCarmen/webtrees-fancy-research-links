3edb373b7e6bd7ecf6ce8a56ab96cf20ecf4ccc2
clivej <47480488+clivej@users.noreply.github.com>
2023-10-28 23:14:51 +0100

    Add plugin's for Ireland and Northern Ireland
    Adding some Research links to resources for people based on the island of Ireland.  Mainly the Civil Registration Records (births, deaths and marriages), the 1901 and 1911 Census of Ireland, Griffith Valuation, Tithe Appointment Books and sites listing deaths on the island mainly rip.ie and funeraltimes.com.

---
+++ GriffithValuationPlugin.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class GriffithValuationPlugin extends FancyResearchLinksModule
+{
+    public function pluginLabel(): string
+    {
+        return 'Griffith Valuation';
+    }
+
+    public function pluginName(): string
+    {
+        return strtolower(basename(__FILE__, 'Plugin.php'));
+    }
+
+    public function researchArea(): string
+    {
+        return 'IRL';
+    }
+
+    public function researchLink($attributes): string
+    {
+        // This uses the search syntax from the 2023 version of Griffith Valuation site
+
+        $name = $attributes['NAME'];
+
+        return 'https://www.askaboutireland.ie/griffith-valuation/index.xml?action=doNameSearch&familyname=' 
+        . $name['surname'] . '&wildcard=on&firstname=' 
+        . $name['first'] .'&baronyname=&countyname=&unionname=&parishname=';
+    }
+}
---
+++ PRONIUlsterCovenantPlugin.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class PRONIUlsterCovenantPlugin extends FancyResearchLinksModule
+{
+    public function pluginLabel(): string
+    {
+        return 'Public Records Office Northern Ireland (PRONI) - Ulster Covenant (link only)';
+    }
+
+    public function pluginName(): string
+    {
+        return strtolower(basename(__FILE__, 'Plugin.php'));
+    }
+
+    public function researchArea(): string
+    {
+        return 'NIR';
+    }
+
+    public function researchLink($attributes): string
+    {
+        // 
+
+        $name = $attributes['NAME'];
+
+        return 'https://apps.proni.gov.uk/ulstercovenant/Search.aspx';
+    }
+}
