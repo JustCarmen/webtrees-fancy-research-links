b88a19c7510e4499b985af9d0d161e65b5a3be64
stc4000 <101832939+stc4000@users.noreply.github.com>
2024-01-06 09:47:17 +0100

    Add files via upload

---
+++ plugins/KbhArkivPlugin.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+/**
+ * Fritekstsøgning på efternavn i Københavns Stadsarkiv.
+ * Mest anvendelig med lidt sjældnere efternavne.
+ */
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class KbhArkivPlugin extends FancyResearchLinksModule
+{
+    public function pluginLabel(): string
+    {
+        return 'Københavns Stadsarkiv';
+    }
+
+    public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+    public function researchArea(): string
+    {
+        return 'DNK';
+    }
+
+    public function researchLink($attributes): string
+    {
+    $name = $attributes['NAME'];
+
+        return 'https://kbharkiv.dk/brug-samlingerne/soeg-i-indtastede-kilder/results?q1.f=freetext_store&q1.op=in_multivalued&q1.t=' . $name['surn'];
+    }
+}
