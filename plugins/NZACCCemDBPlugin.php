efd87ff0268c36af10446915a046563c778d6386
RAMINCNZ <RAMINCNZ@users.noreply.github.com>
2024-12-01 12:32:14 +1300

    Various NZ Councils Cem D/base Search Links
    Various New Zealand Councils Cemetery Database search links.
    More will be added as I use them.

---
+++ plugins/NZACCCemDBPlugin.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class NZACCCemDBPlugin extends FancyResearchLinksModule
+{
+	public function pluginLabel(): string
+    {
+		return 'Auckland City Council Cemetery Database';
+	}
+
+	public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public function researchArea(): string
+    {
+		return 'NZL';
+	}
+
+	public function researchLink(): string
+    {
+		return 'https://www.aucklandcouncil.govt.nz/cemeteries/Pages/find-burial-cremation.aspx';
+	}
+}
---
+++ plugins/NZGovtNationalArchivesPlugin.php
@@ -0,0 +1,30 @@
+<?php
+declare(strict_types=1);
+
+namespace JustCarmen\Webtrees\Module\FancyResearchLinks\Plugin;
+
+use Fisharebest\Webtrees\I18N;
+use JustCarmen\Webtrees\Module\FancyResearchLinks\FancyResearchLinksModule;
+
+class NZGovtNationalArchivesPlugin extends FancyResearchLinksModule
+{
+	public function pluginLabel(): string
+    {
+		return 'Search Archives New Zealand';
+	}
+
+	public function pluginName(): string
+	{
+		return strtolower(basename(__FILE__, 'Plugin.php'));
+	}
+
+	public function researchArea(): string
+    {
+		return 'NZL';
+	}
+
+	public function researchLink(): string
+    {
+		return 'https://collections.archives.govt.nz/en/web/arena/search#/';
+	}
+}
